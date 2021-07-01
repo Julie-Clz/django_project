363a93527c28652e27cbfe29e59423c12417b466
julie collazos <juliecollazos@hotmail.com>
2021-07-01 16:55:59 +0200

    Add view user stats

--- users/views.py
+++ users/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
-from pronos.models import Userteam, UserteamMember
+from pronos.models import Userteam, UserteamMember, Bet
+from django.db.models import Sum
 
 
 def register(request):
@@ -22,6 +23,10 @@ def register(request):
 def profile(request):
     userteams =  Userteam.objects.filter(user=request.user).all()
     members =  UserteamMember.objects.filter(user=request.user).all()
+    points = Bet.objects.filter(user=request.user).aggregate(sum_point=Sum('point')).get('sum_point')
+    bets_num = Bet.objects.filter(user=request.user).count()
+    bets_finish = Bet.objects.filter(user=request.user).filter(match__done=True).count()
+    points_max = bets_num * 3
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
@@ -39,7 +44,34 @@ def profile(request):
         'u_form': u_form,
         'p_form': p_form,
         'userteams': userteams,
-        'members': members
+        'members': members,
+        'points_max': points_max,
+        'points': points,
+        'bets_num': bets_num,
+        'bets_finish': bets_finish,
     }
 
     return render(request, 'users/profile.html', context)
+
+
+@login_required
+def statistiques(request):
+    userteams =  Userteam.objects.filter(user=request.user).all()
+    members =  UserteamMember.objects.filter(user=request.user).all()
+    points = Bet.objects.filter(user=request.user).aggregate(sum_point=Sum('point')).get('sum_point')
+    bets = bets_num = Bet.objects.filter(user=request.user).all()
+    bets_num = Bet.objects.filter(user=request.user).count()
+    bets_finish = Bet.objects.filter(user=request.user).filter(match__done=True).count()
+    points_max = bets_num * 3
+    
+    context = {
+        'userteams': userteams,
+        'members': members,
+        'points_max': points_max,
+        'points': points,
+        'bets_num': bets_num,
+        'bets_finish': bets_finish,
+        'bets': bets,
+    }
+
+    return render(request, 'users/user_stats.html', context)
